69b710f17dc6bcbe21167e25b32198848461fdfe
Finn Fancher <fancherfinn@gmail.com>
2024-06-28 12:47:00 -0700

    update main.py

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ def main():
     # Comlete your code here
     ##################################################
 
+    start = int(input("Enter starting number: "))
+    end = int(input("Enter ending number: "))
+    count = 0
+    
+    for i in range(start, end):
+        for j in range(2, 10):
+            if(i%j == 0):
+                count +=1
+        if(count == 0):
+            plist.append(i)
+        count = 0
+            
+    
+    print(plist)
     return plist
 
 
